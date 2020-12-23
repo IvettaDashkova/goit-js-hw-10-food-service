72707a6e368e5ef96c37530927375b9802cc4451
IvettaDashkova <ivettazakonno@gmail.com>
2020-12-24 01:45:38 +0200

    убирает лишние строки

--- src/js/templating.js
+++ src/js/templating.js
@@ -1,8 +1,8 @@
 import cardsTemplate from '../templates/menu-cards.hbs';
 import menuCards from '../menu.json'
-console.log(menuCards); 
+
 
 const markup = cardsTemplate(menuCards)
-console.log(markup);
+
 const menuRef = document.querySelector('.js-menu');
 menuRef.insertAdjacentHTML('beforeend', markup)
